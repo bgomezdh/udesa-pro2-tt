8359535255ccc8a1be1df110e25146121d941506
bgomezdh <80359150+bgomezdh@users.noreply.github.com>
2022-03-15 13:57:50 -0300

    Add folder about funtions

---
+++ day2-funtions/clase/app.js
@@ -0,0 +1,21 @@
+function sumar(a , b) {
+   return a + b;
+}
+
+function restar(a , b) {
+    return a - b;
+ }
+
+ function multiplicar(a , b) {
+    return a * b;
+ }
+
+ function dividir(a , b) {
+    return a / b;
+ }
+
+function calculadora(a , b , restar) {
+    return restar(a , b)
+}
+
+console.log(calculadora( 2, 3, restar));
