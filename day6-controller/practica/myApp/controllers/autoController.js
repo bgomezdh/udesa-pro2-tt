bd83f1df0a8aae06bd8e0728d0d137a84e004905
Brian Gomez <bgomez@DH.digitalhouse.com>
2022-04-04 14:17:27 -0300

    Day6 crear vistas

--- day6-controller/practica/myApp/controllers/autoController.js
+++ day6-controller/practica/myApp/controllers/autoController.js
@@ -2,7 +2,10 @@ const autos = require('../db/autos');
 
 const autoController = {
     index:function(req, res) {
-        return res.send(autos.lista);
+        return res.render('autos', {
+            listaAutos : autos.lista,
+            tipo : "Todos los Autos"
+        } );
       },
       buscarPorColor: function(req,res){
         let autosPorColor = [];
@@ -14,7 +17,10 @@ const autoController = {
             }
         }
         if (autosPorColor.length > 0 ) {
-            return res.send(autosPorColor)
+            return res.render('autos', {
+                listaAutos : autosPorColor,
+                tipo : "Todos los Autos por el color " + colorBuscado
+            })
         } else {
             return res.send('No se encontraron autos del color ' + colorBuscado)
         }
